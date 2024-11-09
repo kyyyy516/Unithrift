65d63101365d6e94a4becf43513d5adf8ddee782
LIM XIAO XUAN <148971311+limxxuan@users.noreply.github.com>
2024-11-09 18:01:19 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'homepage.dart';
+//import 'otp_from.dart';
+import 'wrapper.dart';
 import 'login.dart';
-import 'register.dart'; // Import your Register page
-import 'verification.dart';
+import 'Register.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyB05amp4j-PV4Wfyy1bQwiEDdFbaTPHefY",
+            authDomain: "unithrift-b2282.firebaseapp.com",
+            projectId: "unithrift-b2282",
+            storageBucket: "unithrift-b2282.firebasestorage.app",
+            messagingSenderId: "381941062209",
+            appId: "1:381941062209:web:588230cd0d4741819484c0",
+            measurementId: "G-G6ZR5F2CF3"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -16,12 +35,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/login',
       routes: {
-        '/register': (context) => const Register(),
-        '/verification': (context) => const VerificationPage(),
-        '/login': (context) => const Login(), // Add your LoginPage route here
+      '/register': (context) => const Register(),
+      '/login': (context) => const Login(),
+      '/homepage': (context) => const Homepage(), // Add Homepage route
       },
-      home: const Register(), // Home page
+      home: const Wrapper(), // Home page
     );
   }
 }
-
