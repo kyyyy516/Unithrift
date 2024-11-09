f57006aca7cdc339dcdedda9fcd83891139367fa
yunneyi <yunyimok@gmail.com>
2024-11-09 22:46:47 +0800

    text checked from yunyi

---
+++ lib/category/category_search.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchCategoty extends StatefulWidget {
+  const SearchCategoty({super.key});
+
+  @override
+  State<SearchCategoty> createState() => _SearchCategotyState();
+}
+
+class _SearchCategotyState extends State<SearchCategoty> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: const Text("test"));
+  }
+}
