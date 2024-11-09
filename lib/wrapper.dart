9402e9c26ff800876d048711bf715a85c9a3999a
LIM XIAO XUAN <148971311+limxxuan@users.noreply.github.com>
2024-11-09 18:03:01 +0800

    Update wrapper.dart

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,3 +1,4 @@
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'homepage.dart';
@@ -14,15 +15,19 @@ class _WrapperState extends State<Wrapper> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return const Homepage();
-            } else {
-              return const Login();
-            }
-          }),
+      body: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+    if (snapshot.hasData) {
+      // User is signed in
+      return const Homepage();
+    } else {
+      // User is not signed in
+      return const Login();
+      }
+      },
+      )
     );
   }
 }
+
